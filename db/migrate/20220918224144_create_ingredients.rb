938809d76aea8680273fa86a719ca3139f389b17
Thomas Hayes <hayes.thomas610@gmail.com>
2022-09-18 18:06:21 -0600

    rollback and migrate correct database and begin story 1

--- db/migrate/20220918224144_create_ingredients.rb
+++ db/migrate/20220918224144_create_ingredients.rb
@@ -1,7 +1,6 @@
 class CreateIngredients < ActiveRecord::Migration[5.2]
   def change
     create_table :ingredients do |t|
-      t.references :dish, foreign_key: true
       t.string :name
       t.integer :calories
 
---
+++ spec/features/dish/show_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe 'Dish Show Page' do
+  describe 'When I visit a dish show page' do
+    it 'I see the dish’s name and description' do
+      @tom = Chef.create!(name: "Tom")
+
+      @toast = @tom.dishes.create!(name: "toast", description: "avacado toast")
+    
+      @bread = Ingredient.create!(name: "bread", calories: 100)
+      @avacado = Ingredient.create!(name: "avacado", calories: 50)
+
+      DishIngredient.create!(dish_id: @toast.id, ingredient_id: @bread.id)
+      DishIngredient.create!(dish_id: @toast.id, ingredient_id: @avacado.id)
+
+      visit "dishes/#{@toast.id}"
+
+      expect(page).to have_content("Name: toast")
+      expect(page).to have_content("Description: avacado toast")
+      within div
+      expect(page).to have_content("bread")
+      expect(page).to have_content("avacado")
+      expect(page).to have_content("Chef Tom")
+    end
+  end
+end
